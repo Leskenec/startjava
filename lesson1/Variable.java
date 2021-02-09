4b4200c2934b9a5494f155f1892faf59b1597a03
unknown <Leskenec>
2021-02-10 00:28:51 +0700

    Homework1

---
+++ lesson1/Variable.java
@@ -0,0 +1,20 @@
+public class Variable{
+    public static void main(String[] args) {
+        byte b = 100;
+        System.out.println(b);
+        short s = 12000;
+        System.out.println(s);
+        int i = 4343456;
+        System.out.println(i);
+        long l = 345345345;
+        System.out.println(l);
+        float f = 3.233f;
+        System.out.println(f);
+        double d = 3.1234445d;
+        System.out.println(d);
+        char c = 'W';
+        System.out.println(c);
+        boolean k = false;
+        System.out.println(k);
+    }
+}
