f06053d8b9ba37d45066a014a23a2e13929c092a
Mukhamed <Leskenec101@gmail.com>
2021-02-19 15:15:38 +0700

    Add Unicod

--- lesson1/Unicod.java
+++ lesson1/Unicod.java
@@ -1,7 +1,7 @@
 public class Unicod {
     public static void main(String[] args) {
         char partCharacters;
-        for(partCharacters = 33; partCharacters <=126; partCharacters++){
+        for(partCharacters = 33; partCharacters <=126; partCharacters++) {
             System.out.println(partCharacters);
         }
     }
