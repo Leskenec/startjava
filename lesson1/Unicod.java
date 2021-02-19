96aa73f623734857e412a2339b58e7c512d9f1de
Mukhamed <Leskenec101@gmail.com>
2021-02-19 16:31:13 +0700

    Add Unicod (correct)

--- lesson1/Unicod.java
+++
@@ -1,8 +0,0 @@
-public class Unicod {
-    public static void main(String[] args) {
-        char partCharacters;
-        for(partCharacters = 33; partCharacters <=126; partCharacters++) {
-            System.out.println(partCharacters);
-        }
-    }
-}
---
+++ lesson1/Unicode.java
@@ -0,0 +1,7 @@
+public class Unicode {
+    public static void main(String[] args) {
+        for(char symbol = 33; symbol <= 126; symbol++) {
+            System.out.println(symbol);
+        }
+    }
+}
