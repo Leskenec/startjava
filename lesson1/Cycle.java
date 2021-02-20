03df3414ae0ca5e14d65a3cba30f111d356cb4e2
Mukhamed <Leskenec101@gmail.com>
2021-02-20 10:23:46 +0700

    correct mistakes2

--- lesson1/Cycle.java
+++ lesson1/Cycle.java
@@ -4,18 +4,21 @@ public class Cycle {
             System.out.println(i);
         } 
         System.out.println("");
+
         int i = 6;
         while(i >= -6) {
             System.out.println(i);
-            i-=2;
+            i -= 2;
         } 
         System.out.println("");
+        
         int a = 10;  // переменная i уже использовалась
         int sum = 0;
         do {
-            if(a % 2==1) 
-                sum+=a; // sum = sum +a;
+            if(a % 2 == 1) {
+                sum += a; // sum = sum +a;
                 a++;
+            }
         } while (a <= 20);
         System.out.println(sum);
     }
