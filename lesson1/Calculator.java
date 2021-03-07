15b037ba98c60cd36f652c18a0ebda2858cfe5dd
Mukhamed <Leskenec101@gmail.com>
2021-03-07 15:24:50 +0700

    correct mistake

--- lesson1/Calculator.java
+++ lesson1/Calculator.java
@@ -1,20 +1,24 @@
 public class Calculator {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 2;
-        char math = '^';
-        if(math == '+') {
-            System.out.println(a + "+" + b + "=" + (a+b) + " - " + "Сложение");
-        } else if(math == '-') {
-            System.out.println(a + "-" + b + "=" + (a-b) + " - " + "Вычитание");
-        } else if(math == '*') {
-            System.out.println(a + "*" + b + "=" + (a*b) + " - " + "Умножение");
-        } else if(math == '/') {
-            System.out.println(a + "/" + b + "=" + (a/b) + " - " + "Деление");
-        } else if(math == '^') {
-            System.out.println(a + "^" + a + "=" + (a*a) + " - " + "Возведение в степень");
-        } else if(math == '%') {
-            System.out.println(a + "%" + b + "=" + (a%b) + " - " + "Деление по модулю");
+        int a = 3;
+        int b = 4;
+        int result = 1;
+        char mathSymbol = '^';
+        if(mathSymbol == '+') {
+            System.out.println(a + " + " + b + " = " + (a + b) + " - " + "Сложение");
+        } else if(mathSymbol == '-') {
+            System.out.println(a + " - " + b + " = " + (a - b) + " - " + "Вычитание");
+        } else if(mathSymbol == '*') {
+            System.out.println(a + " * " + b + " = " + (a * b) + " - " + "Умножение");
+        } else if(mathSymbol == '/') {
+            System.out.println(a + " / " + b + " = " + (a / b) + " - " + "Деление");
+        } else if(mathSymbol == '^') {
+            for(int n = 0; n < b; n++) {
+                result = result * a;
+            }
+            System.out.println(a + " ^ " + b + " = " + (result) + " - " + "Возведение в степень");
+        } else if(mathSymbol == '%') {
+            System.out.println(a + " % " + b + " = " + (a % b) + " - " + "Деление по модулю");
         }
     }
 }
