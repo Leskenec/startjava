a24682a3d604bd21de57a92b0ff2cc33c670b64a
Mukhamed <Leskenec101@gmail.com>
2021-03-07 18:08:43 +0700

    correct mistake 2

--- lesson1/Calculator.java
+++ lesson1/Calculator.java
@@ -2,8 +2,8 @@ public class Calculator {
     public static void main(String[] args) {
         int a = 3;
         int b = 4;
-        int result = 1;
         char mathSymbol = '^';
+
         if(mathSymbol == '+') {
             System.out.println(a + " + " + b + " = " + (a + b) + " - " + "Сложение");
         } else if(mathSymbol == '-') {
@@ -13,8 +13,9 @@ public class Calculator {
         } else if(mathSymbol == '/') {
             System.out.println(a + " / " + b + " = " + (a / b) + " - " + "Деление");
         } else if(mathSymbol == '^') {
-            for(int n = 0; n < b; n++) {
-                result = result * a;
+            int result = 1;
+            for(int i = 0; i < b; i++) {
+                result *= a;
             }
             System.out.println(a + " ^ " + b + " = " + (result) + " - " + "Возведение в степень");
         } else if(mathSymbol == '%') {
