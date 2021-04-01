46f9bfb9b811776170beeb74df1124e2d38c344a
Mukhamed <Leskenec101@gmail.com>
2021-04-01 12:52:03 +0700

    Add fail Person

---
+++ lesson2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String name = "someone";
+    String gander = "male";
+    int age = 30;
+    int weight = 85;
+    float height = 1.85f;
+
+    void go() {
+        
+    }
+
+    void sit() {
+    }
+
+    void run() {
+        System.out.println("Вы бежите");
+    }
+
+    String speak() {
+        return "words";
+    }
+
+    boolean studyJava() {
+        System.out.println("Вы учите джава");
+        return true;
+    }
+}
