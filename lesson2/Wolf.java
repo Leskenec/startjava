052a6f10e0cb740e55b8089f3c152e0a84c5eafa
Mukhamed <Leskenec101@gmail.com>
2021-04-08 16:01:19 +0700

    correct  wolftest.java

--- lesson2/Wolf.java
+++ lesson2/Wolf.java
@@ -1,24 +1,68 @@
 public class Wolf {
 
-    String gender;
-    String nickname;
-    int weight;
-    int age;
-    String color;
+    private String gender = "male";
+    private String nickname = "wild";
+    private int weight = 23;
+    private int age = 7;
+    private String color = "grey";
 
-    void go() {
+    public String getGender() {
+        return gender;
+    }
+    
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname (String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age > 8) {
+            System.out.println("Некоректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+    
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void go() {
         System.out.println("Волк идет");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("Волк сидит");
     }
 
-    void run() {
+    public void run() {
         System.out.println("Волк бежит");
     }
 
-    boolean howl() {
+    public boolean howl() {
         System.out.println("Выть на луну");
         return true;
     }
--- lesson2/WolfTest.java
+++ lesson2/WolfTest.java
@@ -2,14 +2,16 @@ public class WolfTest {
     public static void main(String[] args) {
         Wolf wild = new Wolf();
 
-        wild.gender = "male";
-        wild.nickname = "wild";
-        wild.age = 5;
-        wild.weight = 23;
-        wild.color = "grey";
-        System.out.println(wild.age);
-        System.out.println(wild.gender);
-        System.out.println(wild.nickname);
+        wild.setGender("Male");
+        wild.setNickname("Barbos");
+        wild.setAge(11);
+        wild.setWeight(27);
+        wild.setColor("white");
+        System.out.println(wild.getGender()); 
+        System.out.println(wild.getNickname());
+        System.out.println(wild.getAge());
+        System.out.println(wild.getWeight());
+        System.out.println(wild.getColor());
         System.out.println(wild.howl());
     }
 }
